292d2def84802988bcd8f785baeb4c50feaac98d
Sarvarbek <makhmudovsx17@gmail.com>
2026-09-12 14:49:05 +0900

    feat: learnn python booleans

--- primitive.py
+++ primitive.py
@@ -28,3 +28,20 @@ result = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result}")
 
 print("==== boolean ====")
+# functions > type(), input(), bool(), int(), str()
+
+y = input("Give your value for y:")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY
+# TRUTHY: True, 100, -100, "MIT"
+# FALSY: False, 0, "", None
+
+test_falsy = "" or False or None or 0
+print(f"test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print(f"test_truthy:", bool(test_truthy))
