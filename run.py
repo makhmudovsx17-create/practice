9195f7c5ddf45c1f7ffbc10a02a2f6fb10d8111d
Sarvarbek <makhmudovsx17@gmail.com>
2026-09-12 13:50:32 +0900

    feat: python sysstem variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
+# Dunder: __builtins__, __init__
 
+message = "PYTHON: Eberything is object!"
+print(message)
+
+result = type(message)
+print("result:", result)
+
+''' In Puthon, there are builtin tools: 
+ (1) TYPES > int, float, str, list, dict
+ (2) FUNCTIONS > print(), len(), inpuy(), type() str() int()
+ (3) CONSTANTS > True, False, None 
+'''
+
+print(dir(__builtins__))
